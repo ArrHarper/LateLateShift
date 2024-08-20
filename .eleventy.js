1777d3133db369137508d55680b938b31f15f544
ArrHarper <harper@wistia.com>
2024-08-20 00:37:32 -0400

    Major Update: site redesigned with Eleventy/Nunjucks templates

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    // Copy static assets directly to the output
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+  
+    return {
+      dir: {
+        input: "src",
+        output: "_site",
+        includes: "_includes"
+      },
+      templateFormats: ["html", "njk", "md"],
+      htmlTemplateEngine: "njk",
+      markdownTemplateEngine: "njk",
+      dataTemplateEngine: "njk"
+    };
+  };
