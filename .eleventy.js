ddcd9a1dea631aa77fd303f250c96e77d57c9803
ArrHarper <harper@wistia.com>
2024-08-27 11:53:14 -0400

    various backend updates, add draft order page

--- .eleventy.js
+++ .eleventy.js
@@ -4,17 +4,18 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/images");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
-  
+ 
     return {
-      pathPrefix: "/LateLateShift/",
-      dir: {
-        input: "src",
-        output: "_site",
-        includes: "_includes"
-      },
-      templateFormats: ["html", "njk", "md"],
-      htmlTemplateEngine: "njk",
-      markdownTemplateEngine: "njk",
-      dataTemplateEngine: "njk",
+        pathPrefix: "/LateLateShift/",
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_includes",
+            data: "_data"
+        },
+        templateFormats: ["html", "njk", "md"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
     };
-  };
+};
